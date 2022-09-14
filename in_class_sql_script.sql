62165365e48b7e0dcf563890a5319ac522f00bc7
Anastasia Jones <anastasiajones@Anastasias-MBP.attlocal.net>
2022-09-14 11:39:46 -0500

    Got rid of lines with wrong date time code

--- in_class_sql_script.sql
+++ in_class_sql_script.sql
@@ -113,25 +113,9 @@ customer_id INT(4) REFERENCES customer (customer_id),
 salesman_id INT(4) REFERENCES salesman (salesman_id));
 
 
-
-
-INSERT INTO salesorder
-(order_no, purch_amt, ord_date, customer_id, salesman_id)
-VALUES
-(70001, 150.5, 2012-10-05, 3005, 5002),
-(70009, 270.65, 2012-09-10, 3001, 5005),
-(70002, 65.26, 2012-10-05, 3002, 5001),
-(70004, 110.5, 2012-08-17, 3009, 5003),
-(70007, 948.5, 2012-09-10, 3005, 5002),
-(70005, 2400.6, 2012-07-27, 3007, 5001),
-(70008, 5760, 2012-09-10, 3002, 5001),
-(70012, 250.45, 2012-06-27, 3008, 5002),
-(70011, 75.29, 2012-08-17, 3003, 5007),
-(70013, 3045.6, 2012-04-25, 3002, 5001); 
-
-
-DELETE FROM salesorder
-WHERE order_no>70000;
+-- How to delete table info, pick a value and do all values from it  
+-- DELETE FROM salesorder
+-- WHERE order_no>70000;
 
 
 INSERT INTO salesorder
